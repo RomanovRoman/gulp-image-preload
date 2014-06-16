470ac9f9b5bd764962e225b2d37988564413217e
Efremov Alexey <alexey.efremov@saltpepper.ru>
2014-06-16 12:09:57 +0400

    generate js content && cover simple test

---
+++ index.js
@@ -0,0 +1,75 @@
+var through2 = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var path = require('path');
+var _ = require('lodash');
+var fs = require('fs');
+
+module.exports = function (options) {
+  options = _.defaults(options || {},{
+    jsvar: "PRELOADER",
+    injectFile: null,
+    rev: false,
+    reduceRev: function(filename) {
+      return filename.replace(/([^\.]+)\.(.+)/, "$2");
+    }
+  });
+  if (options.inlineLoad === null) {
+    options.inlineLoad = options.inlineFile;
+  }
+
+  var buffer = {};
+
+  function processData(file, enc, next){    
+    var self = this;
+    if (file.isNull()) {
+      this.push(file); // pass along
+      return next();
+    }
+    if (file.isStream()) {
+      this.emit('error', new PluginError('gulp-image-preload', 'Streaming not supported'));
+      return next();
+    }
+    var filename = path.normalize(file.path);
+    var pieces = filename.split(path.sep);
+    var pointer = _.reduce(pieces.slice(0, pieces.length - 1), (function(pointer, item) {
+      return pointer[item] || (pointer[item] = {});
+    }), buffer);
+    filename = pieces[pieces.length - 1];
+    var processFilename = options.rev ? options.reduceRev(filename) : filename;
+    pointer[processFilename] = filename;
+    next();
+  }
+  function endStream(){
+    var self = this;
+    var content = JSON.stringify(buffer);    
+    fs.readFile("template/inject.min.js",function(err, data){
+      if(!!err){
+        return self.emit('error',err);
+      }
+      var fileData = data.toString();  
+      fileData = fileData.replace(/window\.PRELOADER[ ]*=/, "");
+      var script = "window." + options.jsvar + " = " + fileData + "; window." + options.jsvar + "=window." + options.jsvar + "(" + content + ");";
+      
+      if(options.injectFile === null){
+        var result = "<!--preloader:js--><script> " + script + " </script><!--endpreloader:js--></head>";
+        self.emit('data',result);
+        self.emit('end');
+      } else {
+        var rx = /<[ ]*\/[ ]*head[ ]*>/;
+        var rxClean = /<!--[ ]*preloader:js[ ]*-->.+<!--[ ]*endpreloader:js[ ]*-->/;
+        fs.readFile(options.injectFile, function(err, html){
+          if(!!err){
+            return self.emit('error',err);
+          }
+          result = html.replace(rxClean, "").replace(rx, result);
+          self.emit('data',result);
+          self.emit('end');
+        });
+      }
+    });
+    
+  }
+
+  return through2.obj({}, processData, endStream);
+};
---
+++ tests/main-spec.js
@@ -0,0 +1,62 @@
+var should = require('should');
+var imagepreload = require('../');
+var gutil = require('gulp-util');
+var fs = require('fs');
+var path_join = require('path').join;
+var vfs = require('vinyl-fs');
+
+function imageFile(imagename){
+  var base = path_join(__dirname,"fixtures");
+  var path = path_join(base, imagename);
+  return new gutil.File({
+    cwd: __dirname,
+    base: base,
+    path: path,
+    contents: fs.readFileSync(path)
+  })
+}
+
+describe('gulp-image-preload',function(){
+  describe("imagepreload()",function(){
+    it('emptyFile',function(done){
+      var stream = imagepreload();
+      var emptyFile = {
+        isNull: function(){ return true; }
+      }
+      stream.on("data",function(data){
+        data.should.equal(emptyFile);
+        done();
+      })
+      stream.write(emptyFile);
+    });
+    it('fail stream',function(done){
+      var stream = imagepreload()
+      var streamFile = {
+        isNull: function(){ return false; },
+        isStream: function(){ return true; }
+      };
+      stream.on('error',function(err){
+        err.message.should.equal("Streaming not supported");
+        done();
+      });
+      stream.write(streamFile);
+    });
+    
+    it('test config',function(done){
+      var pattern = path_join(__dirname, "fixtures/*.jpeg");
+      vfs
+        .src(pattern)
+        .pipe(imagepreload())
+        .on('data',function(info){          
+          should.exist(info);
+          should.equal(info.indexOf('<!--preloader:js-->'), 0);
+          should.exist(info.indexOf('<!--endpreloader:js-->') > 0 );
+          should.equal(info.indexOf('</head>'), info.length - 7);
+          should.exist(info.indexOf("window.PRELOADER") > 0 );
+          should.exist(info.indexOf('cat1.jpeg') > 0);
+          should.exist(info.indexOf('cat2.jpeg') > 0);
+        })
+        .on('end', done);
+    });
+  });  
+});
